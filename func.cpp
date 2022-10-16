3cb95a6dada2a81283d368615479089489ea3954
Calvin <CalvinCui@foxmail.com>
2022-10-16 21:00:58 +0800

    use pos macro

--- func.cpp
+++ func.cpp
@@ -28,12 +28,14 @@ void LoopRowMajorOrderingPre(FUNC_PARAM_PRE) {
 	*pC = ColMajor2RowMajor(M, N, C);
 }
 
+#define pos(mat, i, j, ld) (mat)[(i) * (ld) + (j)]
+
 void LoopRowMajorOrderingPro(FUNC_PARAM_PRO) { // 三重循环 + 所有元素按行排序
 	for (int i = 0;i < M;i++) {
 		for (int j = 0;j < N;j++) {
-			C[i * ldc + j] *= beta;
+			pos(C, i, j, ldc) *= beta;
 			for (int k = 0;k < K;k++) {
-				C[i * ldc + j] += alpha * A[i * lda + k] * B[k * ldb + j];
+				pos(C, i, j, ldc) += alpha * pos(A, i, k, lda) * pos(B, k, j, ldb);
 			}
 		}
 	}
@@ -47,13 +49,13 @@ void LoopRowMajorOrderingPost(FUNC_PARAM_POST) {
 
 // LoopRowMajorBlocking
 
-#define BLOCK_SIZE 5
-
 void LoopRowMajorBlockingPre(FUNC_PARAM_PRE) {
 	*pA = ColMajor2RowMajor(M, K, A);
 	*pB = ColMajor2RowMajor(K, N, B);
 	*pC = ColMajor2RowMajor(M, N, C);
 }
+
+#define pos(mat, ih, il, jh, jl, ld) (mat)[((ih) + (il)) * (ld) + (jh) + (jl)]
 
 void LoopRowMajorBlockingPro(FUNC_PARAM_PRO) {
 	for (int ih = 0;ih < M;ih += BLOCK_SIZE) {
@@ -61,9 +63,9 @@ void LoopRowMajorBlockingPro(FUNC_PARAM_PRO) {
 			for (int kh = 0;kh < K;kh += BLOCK_SIZE) {
 				for (int il = 0;il < BLOCK_SIZE && ih + il < M;il++) {
 					for (int jl = 0;jl < BLOCK_SIZE && jh + jl < N;jl++) {
-						C[(ih + il) * ldc + jh + jl] *= kh ? 1 : beta; // 控制使beta只算一次
+						pos(C, ih, il, jh, jl, ldc) *= kh ? 1 : beta; // 控制使beta只算一次
 						for (int kl = 0;kl < BLOCK_SIZE && kh + kl < K;kl++) {
-							C[(ih + il) * ldc + jh + jl] += alpha * A[(ih + il) * lda + kh + kl] * B[(kh + kl) * ldb + jh + jl];
+							pos(C, ih, il, jh, jl, ldc) += alpha * pos(A, ih, il, kh, kl, lda) * pos(B, kh, kl, jh, jl, ldb);
 						}
 					}
 				}
--- func.h
+++ func.h
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <cstring>
 
+#define BLOCK_SIZE 20
+
 #define FUNC_PARAM_PRE int M, int N, int K, const double* A, const double* B, const double* C, double** pA, double **pB, double** pC
 #define FUNC_PARAM_PRO int M, int N, int K, const double* A, const double* B, double* C, double alpha, double beta, int lda, int ldb, int ldc
 #define FUNC_PARAM_POST int M, int N, double* C, double *C_
