c0ea1309fff769fb4b452f5a9898fa9d020572f9
Calvin <CalvinCui@foxmail.com>
2022-10-25 20:31:21 +0800

    use __restrict

--- func.h
+++ func.h
@@ -4,7 +4,7 @@
 #include <cstring>
 
 #define FUNC_PARAM_PRE int M, int N, int K, const double* A, const double* B, const double* C, double** pA, double **pB, double** pC, double beta
-#define FUNC_PARAM_PRO int M, int N, int K, const double* A, const double* B, double* C, double alpha, int lda, int ldb, int ldc
+#define FUNC_PARAM_PRO int M, int N, int K, const double* __restrict A, const double* __restrict B, double* __restrict C, double alpha, int lda, int ldb, int ldc
 #define FUNC_PARAM_POST int M, int N, double* C, double *C_
 
 #define FUNC_DECLARE(NAME) void NAME##Pre(FUNC_PARAM_PRE); void NAME##Pro(FUNC_PARAM_PRO); void NAME##Post(FUNC_PARAM_POST);
